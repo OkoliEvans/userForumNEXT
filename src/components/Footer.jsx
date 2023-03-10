61ba2c89ba270f9e875b78616e4ae4504a4d00a2
Evans007 <ebounce500@gmail.com>
2023-03-11 00:45:22 +0100

    tried to fix grid style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Footer = () => {
   return (
     
-    <div className='bg-gray-200 py-4 px-2 text-blue-400 text-xl flex justify-center'>
+    <div className='bg-gray-200 py-4 px-2 text-teal-800 text-sm flex justify-center'>
       Copyright &copy; 2023
     </div>
   )
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -8,13 +8,13 @@ import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export const users = [
-  { id: 1, name: "Ade Moses", occupation: "Developer", gender: 'male' },
-  { id: 2, name: "Dera", occupation: "Developer", gender: 'female' },
-  { id: 3, name: "Adaku", occupation: "trader", gender: 'female' },
-  { id: 4, name: "Benson Moses", occupation: "Developer", gender: 'male' },
-  { id: 5, name: "Mosan", occupation: "Developer", gender: 'male' },
-  { id: 6, name: "Brylon Mex", occupation: "Developer", gender: 'male' },
-  { id: 7, name: "Nodan", occupation: "Developer", gender: 'male' }
+  { id: 1, name: "Ade Moses", occupation: "Developer", gender: "male" },
+  { id: 2, name: "Dera", occupation: "Developer", gender: "female" },
+  { id: 3, name: "Adaku", occupation: "trader", gender: "female" },
+  { id: 4, name: "Benson Moses", occupation: "Developer", gender: "male" },
+  { id: 5, name: "Mosan", occupation: "Developer", gender: "male" },
+  { id: 6, name: "Brylon Mex", occupation: "Developer", gender: "male" },
+  { id: 7, name: "Nodan", occupation: "Developer", gender: "male" },
 ];
 
 const Users = () => {
@@ -30,26 +30,39 @@ const Users = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PageLayouts>
-        <div className={styles.main}>This is User page.
-        
-            {
-                Array.isArray(users) && users.length > 0 ? 
-                users.map(user => {
-                    return (
-                        <Link href={`/users/${user.id}`} className="col-span-1" key={user.id}>
-                            <div><span>Name:</span> <span>{user.name}</span></div>
-                            <div><span>occupation: </span> <span>{user.occupation}</span></div>
-                            <div><span>Gender: </span> <span>{user.gender}</span></div>
-                        </Link>
-                    )
-                }) : (
-                  <> DISPLAY NOT FOUND </>
-                )
-            }
-
+        <div className="">
+         <div className="text-3xl text-teal-800 p-2">This is User page.</div> 
+          {Array.isArray(users) && users.length > 0 ? (
+            users.map((user) => {
+              return (
+                <div className="grid grid-cols-4 w-full">
+                  <div className=" bg-gray-100 m-4">
+                  <Link
+                    href={`/users/${user.id}`}
+                    className="col-span-1"
+                    key={user.id}
+                  >
+                    <div className="">
+                      <span>Name:</span> <span>{user.name}</span>
+                    </div>
+                    <div>
+                      <span>occupation: </span> <span>{user.occupation}</span>
+                    </div>
+                    <div>
+                      <span>Gender: </span> <span>{user.gender}</span>
+                    </div>
+                  </Link>
+                </div>
+                </div>
+                
+              );
+            })
+          ) : (
+            <> DISPLAY NOT FOUND </>
+          )}
         </div>
       </PageLayouts>
     </>
   );
-}
+};
 export default Users;
